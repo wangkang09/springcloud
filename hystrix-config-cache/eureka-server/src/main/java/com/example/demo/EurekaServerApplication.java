ccff84e802dca4ac773c4adc2626efbac62bcb63
wangkang <wangkang_0902@163.com>
2018-07-16 14:46:54 +0800

    1

--- hystrix-config-cache/eureka-server/src/main/java/com/example/demo/EurekaServerApplication.java
+++ hystrix-config-cache/eureka-server/src/main/java/com/example/demo/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.waylau.spring.cloud.weather;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class Application {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
