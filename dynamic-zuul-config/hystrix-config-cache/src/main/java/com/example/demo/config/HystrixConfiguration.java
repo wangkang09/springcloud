a658e6dc096e72c5882f7b11708390d8e5b9e2e1
wangkang <wangkang_0902@163.com>
2018-07-16 14:50:38 +0800

    服务消费者

---
+++ dynamic-zuul-config/hystrix-config-cache/src/main/java/com/example/demo/config/HystrixConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description:这个版本的servletRegistrationBean要自己配置
+ * @Author: wangkang
+ * @Date: Created in 14:56 2018/7/11
+ * @Modified By:
+ */
+@Configuration
+public class HystrixConfiguration {
+    @Bean
+    public ServletRegistrationBean<HystrixMetricsStreamServlet> getServlet(){
+        HystrixMetricsStreamServlet hystrixMetricsStreamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> servletRegistrationBean = new ServletRegistrationBean();
+        servletRegistrationBean.setServlet(hystrixMetricsStreamServlet);
+        servletRegistrationBean.addUrlMappings("/hystrix.stream");
+        servletRegistrationBean.setName("HystrixMetricsStreamServlet");
+        return servletRegistrationBean;
+    }
+}
