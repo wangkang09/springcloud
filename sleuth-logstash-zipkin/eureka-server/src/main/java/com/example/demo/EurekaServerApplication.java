ed204c160a53b2a073b5800bcb79b928d29f667c
wangkang09 <41175635+wangkang09@users.noreply.github.com>
2019-02-20 09:57:49 +0800

    x

--- sleuth-logstash-zipkin/eureka-server/src/main/java/com/example/demo/EurekaServerApplication.java
+++ sleuth-logstash-zipkin/eureka-server/src/main/java/com/example/demo/EurekaServerApplication.java
@@ -1,14 +1,39 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
-@SpringBootApplication
-@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		LinkedList<String> s = new LinkedList<>();
+		s.peek();
+		s.poll();
+		s.remove("1");
+		try {
+			Thread.sleep(10);
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		synchronized (args){
+			try {
+				args.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		ReentrantLock r = new ReentrantLock();
+
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int k = sc.nextInt();
+
+
+
 	}
 }
